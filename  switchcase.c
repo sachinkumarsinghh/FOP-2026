f12ae47f715666ee36989b4fc057be009695cb76
sachinkumarsinghh <sachin16082006@gmail.com>
2026-01-31 07:05:35 +0000

    new file

---
+++  switchcase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int day =2;
+    switch (day){
+        case1:
+        printf("monday\n");
+        break;
+        case2:
+        printf("tuesday\n");
+        break;
+        case3:
+        printf("wednesday\n");
+        break;
+        default:
+        printf("thrusday\n");
+    }
+    return 0;
+     int a[2] [2],i,j;
+     printf("enter the elements");
+     for(i=1,i<3)
+
+
+    
+}
